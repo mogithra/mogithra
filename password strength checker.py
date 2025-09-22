536abbe8b24898ac9d11193f9f2d60860bf3713a
mogithra <mogithranand@gmail.com>
2025-09-22 19:25:36 +0530

    Add files via upload
    The program demonstrates how to use regular expressions (re module) in Python to validate passwords. It applies different rules (length ≥8, uppercase, lowercase, digits, special characters), adds scores, and finally classifies password strength.

---
+++ password strength checker.py
@@ -0,0 +1,21 @@
+import re
+
+password = input("Enter password: ").strip()
+score = 0
+
+# Check length
+if len(password) >= 8: score += 5
+else: print("Too short (<8 chars)")
+
+# Check uppercase, lowercase, digits, special chars
+if re.search(r"[A-Z]", password): score += 6
+else: print("Missing uppercase")
+if re.search(r"[a-z]", password): score += 6
+else: print("Missing lowercase")
+if re.search(r"\d", password): score += 2
+else: print("Missing digit")
+if re.search(r"\W", password): score += 2
+else: print("Missing special char")
+
+print("Final Score:", score)
+print("Strength:", "Strong " if score >= 15 else "Medium " if score >= 10 else "Weak ")
